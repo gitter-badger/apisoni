94422b664b4f2b513f8b1b574dc01166f0350227
Alberto Díaz <alberto@diazweb.net>
2016-08-17 19:57:49 +0200

    docs(apidoc): Added apidoc :)

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser';
+import express from 'express';
 
 module.exports = app => {
   app.set('port', 3000);
   app.use(bodyParser.json());
+  app.use(express.static('public'));
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 module.exports = app => {
+  /**
+   *  @api {get} / API Status
+   *  @apiGroup Status
+   *  @apiSuccess {String} status API Status' message
+   *  @apiSuccessExample {json} Success
+   *      HTTP/1.1 200 OK
+   *      {"status": "Apisoni API"}
+   */
   app.get('/', (req, res) => {
     res.json({ status: 'Apisoni is working :)' });
   });
 };
+
