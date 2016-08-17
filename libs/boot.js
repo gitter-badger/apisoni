d37924fb50d80f18539e10c9b954ab747e57cd12
Alberto Díaz <alberto@diazweb.net>
2016-08-17 11:53:19 +0200

    refactor(layout): Created the basic apisoni layout, added index route with testing, Travis-ci now wo

---
+++ libs/boot.js
@@ -0,0 +1,7 @@
+module.exports = app => {
+    if (process.env.NODE_ENV !== "test") {
+        app.listen(app.get("port"), () => {
+            console.log(`Apisoni - Port ${app.get("port")}`);
+        });
+    }
+};
