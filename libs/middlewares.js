9dadbc32f3d25c1918af336f5b2e89a422ef1ef9
Alberto Díaz <alberto@diazweb.net>
2016-08-17 20:12:51 +0200

    feat(Helmet): Added helmet module to improve api security

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,8 +1,10 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import helmet from 'helmet';
 
 module.exports = app => {
   app.set('port', 3000);
+  app.use(helmet());
   app.use(bodyParser.json());
   app.use(express.static('public'));
 };
