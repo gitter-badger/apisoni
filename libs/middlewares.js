6fac8324612dad951ffc639527bcab0af2b9085e
Alberto Díaz <alberto@diazweb.net>
2016-08-17 20:21:47 +0200

    feat(compression): Added compression module

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import compression from 'compression';
 import helmet from 'helmet';
 
 module.exports = app => {
   app.set('port', 3000);
   app.use(helmet());
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(express.static('public'));
 };
